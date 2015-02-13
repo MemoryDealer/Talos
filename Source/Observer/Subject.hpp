6d8f06746b1a5009929d89c02bd10d9b8b2372af
unknown <unixunited@live.com>
2015-02-13 01:23:05 -0600

    GPLv3

--- Source/Observer/Subject.hpp
+++ Source/Observer/Subject.hpp
@@ -1,9 +1,19 @@
 // ========================================================================= //
-// * Engine
-// Copyright (C) 2015 Jordan Sparks. All Rights Reserved.
-// Unauthorized copying of this file, via any medium is strictly prohibited.
-// Proprietary and confidential.
-// Written by Jordan Sparks <unixunited@live.com> January 2015.
+// Talos - A 3D game engine with network multiplayer.
+// Copyright(C) 2015 Jordan Sparks <unixunited@live.com>
+//
+// This program is free software; you can redistribute it and / or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 // ========================================================================= //
 // File: Subject.hpp
 // Author: Jordan Sparks <unixunited@live.com>
